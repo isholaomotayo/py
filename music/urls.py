3dd53c7b7a161b7a1bdfcfba552685a2c4a9f0c2
Omotayo ISHOLA <isholaomotayo@yahoo.com>
2017-05-22 18:11:16 +0100

    First start

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    #/music/
+    url(r'^$', views.index, name='index'),
+
+    #/music/{id}
+    url(r'^(?P<album_id>[0-9+])/$', views.detail, name='details'),
+
 ]
--- music/views.py
+++ music/views.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
+
 
 # Create your views here.
 def index(request):
-     return HttpResponse("<h1>Welcome to Music world</h1>")
+    all_albums = Album.objects.all()
+    html = '<h1> Welcome to Music </h1> <br><style> img { margin:10px ; float:left;  display: inline-block;} </style>'
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + '<img width=200 src="' + album.album_logo + '"/>' + '</a>'
+    return HttpResponse(html)
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2> Details for Album ID: " + str(album_id) + "</h2>")
